3106255a049894bb75a5bb53907aa494097b5e30
everydayHo <jungwoopage@gmail.com>
2021-12-13 12:51:53 +0900

    JS Home_contact

--- main.js
+++ main.js
@@ -22,7 +22,18 @@ navbarMenu.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+//  handle click on "contact me" button on home
+const contact = document.querySelector(".home_contact");
+
+contact.addEventListener("click", (event) => {
+  const scrollTo = document.querySelector("#contact");
+  scrollTo.scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
