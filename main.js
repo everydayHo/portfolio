2eddff9d7fa4b33b24d80f696b382919f21b6b3f
everydayHo <jungwoopage@gmail.com>
2021-12-13 11:24:39 +0900

    js navbar

--- main.js
+++ main.js
@@ -3,12 +3,26 @@
 const navbar = document.getElementById("navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
+// make navar  transparent when it work
 document.addEventListener("scroll", () => {
-  // console.log(window.scrollY);
-  // console.log(navbarHeight);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+
+// Handle scrollong when tappong on the navbar menu
+
+const navbarMenu = document.querySelector(".navbar_menu");
+
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 });
